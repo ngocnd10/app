3e49fec2f7468eaf3196125b8c55fbb15a378fa1
Ngoc Nguyen Duy <ngocnd10@gmail.com>
2018-07-10 00:38:04 +0700

    Optimized Import

--- src/main/java/com/ngocnd/app/config/SwaggerConfig.java
+++ src/main/java/com/ngocnd/app/config/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package com.ngocnd.app.config;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
